d442753a552332cc6b250e835062fc5c8aac46bf
andrewkolos <andrewrkolos@gmail.com>
2019-05-15 19:34:49 -0500

    add VS Code launch file

--- __tests__/main.spec.ts
+++
@@ -1,8 +0,0 @@
-import { Vector3 } from '../src/main';
-
-test('vector (1,2,3) + scalar 1 = vector (2,3,4)', () => {
-  const vector1 = new Vector3(1,2,3)
-  const target = new Vector3(2,3,4)
-  const sum = vector1.add(1)
-  expect(sum.equals(target)).toBeTruthy()
-})
