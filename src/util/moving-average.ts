07a3374829bacf35cb2c0915b3b4c13e1329faa2
andrewkolos <andrewrkolos@gmail.com>
2019-08-07 23:53:21 -0400

    rename running average to moving average

---
+++ src/util/moving-average.ts
@@ -0,0 +1,27 @@
+export class MovingAverage {
+  private _value = 0;
+  private count = 0;
+  constructor(private numberOfSamplesToAverageOver: number) {
+    if (numberOfSamplesToAverageOver < 1) {
+      throw RangeError(`Number of samples to average over must be at least 1. Received ${numberOfSamplesToAverageOver}.`);
+    }
+  }
+
+  public get value() {
+    return this._value;
+  }
+
+  public add(val: number, ...more: number[]): this {
+
+    const vals: number[] = [val, ...more];
+
+    const sumVals = vals.reduce((p: number, c: number) => p + c);
+
+    const n = (Math.min(this.numberOfSamplesToAverageOver, this.count + vals.length));
+    this._value = this._value * (n - vals.length)/ n + sumVals / n;
+
+    this.count += vals.length;
+
+    return this;
+  }
+}
