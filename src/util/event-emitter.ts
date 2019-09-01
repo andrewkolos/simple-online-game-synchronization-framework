0bdf07719514271ec8dd443db8dfcc8ded7f191c
Andrew Kolos <andrewrkolos@gmail.com>
2019-09-01 18:35:00 -0400

    remove long-alias methods, move implementations to short-alias methods

--- src/util/event-emitter.ts
+++ src/util/event-emitter.ts
@@ -1,5 +1,3 @@
-// tslint:disable
-
 type Arguments<T> = [T] extends [(...args: infer U) => any]
   ? U
   : [T] extends [void] ? [] : [T];
@@ -9,41 +7,25 @@ type Arguments<T> = [T] extends [(...args: infer U) => any]
  * features specifying types for event names and their callbacks.
  */
 export class TypedEventEmitter<Events> {
-  private handlers: Array<any> = [];
+  private handlers: any[] = [];
 
-  public addEventListener<E extends keyof Events>(type: E, fn: Events[E]) {
+  public on<E extends keyof Events>(type: E, fn: Events[E]) {
     this.handlers.push([type, fn]);
   }
 
-  public removeEventListener<E extends keyof Events>(type: E, fn: Events[E]) {
-    this.handlers = this.handlers.filter(handler => 
-      !(handler[0] === type && (fn != null ? true: handler[1] == fn))
-    )
-  }
-
-  public dispatchEvent<E extends keyof Events>(type: E, ...args: Arguments<Events[E]>) {
-    this.handlers.filter(handler => handler[0] === type).forEach(handler => {
-      handler[1](...args);
-    })
-  }
-
-  public clearEventListeners() {
-    this.handlers = [];
-  }
-
-  public on<E extends keyof Events>(type: E, fn: Events[E]) {
-    this.addEventListener(type, fn);
-  }
-
   public off<E extends keyof Events>(type: E, fn: Events[E]) {
-    this.removeEventListener(type, fn);
+    this.handlers = this.handlers.filter((handler) =>
+      !(handler[0] === type && (fn != null ? true : handler[1] == fn)),
+    );
   }
 
   public emit<E extends keyof Events>(type: E, ...args: Arguments<Events[E]>) {
-    this.dispatchEvent(type, ...args);
+    this.handlers.filter((handler) => handler[0] === type).forEach((handler) => {
+      handler[1](...args);
+    });
   }
 
   public clear() {
-    this.clearEventListeners();
+    this.handlers = [];
   }
 }
